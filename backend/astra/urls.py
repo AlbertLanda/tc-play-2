3d5a8d82450d93a4c0743b70712851ec886326ed
Albert Landa <albertandriu123@gmail.com>
2026-07-10 23:11:03 -0500

    feat: agregar proxy transcoder para Astra

--- backend/astra/urls.py
+++ backend/astra/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import astra_channels
+from .views import astra_channels, astra_proxy_url
 
 
 urlpatterns = [
     path("channels/", astra_channels, name="astra_channels"),
+    path("proxy-url/", astra_proxy_url, name="astra_proxy_url"),
 ]
