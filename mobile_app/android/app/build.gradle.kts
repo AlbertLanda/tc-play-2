b802c3417a1f9ea802d1d0c2361172796e0900b1
Albert Landa <albertandriu123@gmail.com>
2026-08-25 14:47:10 -0500

    fix: align secure storage with Android SDK compatibility

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.tc_play_app"
-    compileSdk = 37
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
